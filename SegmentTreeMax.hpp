ab48c3e8d24ea979f24f129f8dec105a5f64e6ee
Aidan K <34078276+aidankay1@users.noreply.github.com>
2022-07-07 21:31:08 -0400

    create SegmentTreeMax header

--- SegmentTreeMax.hpp
+++ SegmentTreeMax.hpp
@@ -1,4 +1,23 @@
+#include <vector>
+#include <algorithm>
+
 class SegmentTreeMax {
    private:
+    // The segment tree, represented by a 1D vector
+    // The first element is always empty
+    std::vector<int> tree;
+
    public:
+    // Basic array constructor
+    SegmentTreeMax(int *array, int size);
+    // Vector constructor
+    SegmentTreeMax(std::vector<int> vector);
+    // Insert a value into the tree
+    void Insert(int value);
+    // Update the value at the specified index
+    void Update(int index, int value);
+    // Find the index of the specified value, if it exists in the tree
+    unsigned int Search(int value);
+    // Print a string representation of the tree to stdout
+    void Print();
 };
